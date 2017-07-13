74843ef1727d16df7fdd2e6a4d30a4fa63dbdf09
Milagro Teruel <milagro.teruel@gmail.com>
2017-07-13 13:19:08 +0200

    Adding tree visualization for annotated documents

---
+++ tada/data/ann2json.py
@@ -0,0 +1,54 @@
+import argparse
+import json
+import networkx as nx
+
+from networkx.readwrite import json_graph
+
+def read_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename', type=str, help='source file to process')
+    return parser.parse_args()
+
+
+def main():
+    args = read_arguments()
+    filename = args.filename
+    nodes = {}
+    edges = {}
+    roots = []
+    digraph = nx.DiGraph()
+    doc_title = filename.replace('.ann', '')
+    with open(filename, 'r') as file_:
+        for line in file_:
+            if line[0] == 'T':
+                name, label, text = line.split('\t')[:3]
+                nodes[name] = {'label': label.split(' ')[0], 'text': text}
+                digraph.add_node(
+                    name, {'label': label.split(' ')[0], 'text': text})
+                if 'major-claim' in label:
+                    roots.append(name)
+            elif line[0] == 'R':
+                name, label = line.split('\t')[:2]
+                label, destination, origin = label.split(' ')
+                origin = origin.replace('Arg2:', '')
+                destination = destination.replace('Arg1:', '')
+                edges[name] = {
+                    'label': label, 'origin': origin,
+                    'destination': destination
+                }
+                digraph.add_edge(origin, destination, {'label': label})
+    data = {'name': doc_title, 'children': []}
+    for root in roots:
+        descendants = nx.descendants(digraph, root)
+        descendants.add(root)
+        subgraph = digraph.subgraph(descendants)
+        if not nx.is_tree(subgraph):
+            continue
+        data['children'].append(json_graph.tree_data(
+            subgraph, root=root, attrs={'children': 'children', 'id': 'name'}))
+    with open('{}.json'.format(doc_title), 'w') as jf:
+        json.dump(data, jf)
+
+
+if __name__ == '__main__':
+    main()
---
+++ tada/verticalTree/collapsibleTree.js
@@ -0,0 +1,192 @@
+
+var viewerWidth = $(document).width();
+var viewerHeight = $(document).height();
+
+var margin = {top: 20, right: 120, bottom: 20, left: 120},
+    width = viewerWidth - margin.right - margin.left,
+    height = viewerHeight - margin.top - margin.bottom;
+
+var nodeHeight = 200, nodeWidth = 160, collapsedWidth = 40,
+    collapsedHeight = 20;
+
+var rootPosition = {x: margin.left + width / 2, y: margin.top};
+
+var maxTextLenght = 50;
+
+var i = 0,
+    duration = 750,
+    root;
+
+function getNodeWidth(d) {
+  return d._children ? collapsedWidth : nodeWidth;
+}
+
+function getNodeHeight(d) {
+  return d._children ? collapsedHeight : nodeHeight;
+}
+
+function getNodeText(d) {
+  if (d._children !== undefined && d._children !== null) {
+    return d.name;
+  }
+  if (d.label && d.text) {
+    return d.name + ' ' + d.label + ' ' + d.text.slice(0, maxTextLenght);
+  }
+  return d.name;
+}
+
+var tree = d3.layout.tree()
+    .nodeSize([nodeWidth * 1.1, nodeHeight])
+    //.size([height, width]);
+
+var diagonal = d3.svg.diagonal()
+    .source(function(d) {
+        return {x: d.source.x, y: d.source.y + nodeWidth};
+    })
+    .projection(function(d) { return [d.x, d.y]; });
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.right + margin.left)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + rootPosition.x + "," + rootPosition.y + ")");
+
+d3.json("sample.json", function(error, dataRoot) {
+  if (error) throw error;
+
+  root = dataRoot;
+  root.x0 = 0;
+  root.y0 = height / 2;
+
+  function collapse(d) {
+    if (d.children) {
+      d._children = d.children;
+      d._children.forEach(collapse);
+      d.children = null;
+    }
+  }
+
+  update(root);
+});
+
+d3.select(self.frameElement).style("height", "800px");
+
+function update(source) {
+
+  // Compute the new tree layout.
+  var nodes = tree.nodes(root).reverse(),
+      links = tree.links(nodes);
+
+  // Normalize for fixed-depth.
+  nodes.forEach(function(d) { d.y = d.depth * nodeHeight * 1.1; });
+
+  // Update the nodes…
+  var node = svg.selectAll("g.node")
+      .data(nodes, function(d) { return d.id || (d.id = ++i); });
+
+  // Enter any new nodes at the parent's previous position.
+  var nodeEnter = node.enter().append("g")
+      .attr('width', nodeWidth)
+      .attr("class", "node")
+      .attr("transform", function(d) { return "translate(" + source.x0 + "," + source.y0 + ")"; })
+      .on("click", click);
+
+  nodeEnter.append("rect")
+      .attr('y', 0)
+      .classed('shape', true)
+      .attr('x', function(d) { return -getNodeWidth(d) / 2 })
+      .attr('height', getNodeHeight)
+      .attr('width', getNodeWidth)
+      .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
+
+  nodeEnter.append("foreignObject")
+      .attr("width", getNodeWidth)
+      .attr("height", getNodeHeight)
+      .attr('class', 'nodeText')
+      .attr('x', function(d) { return -getNodeWidth(d) / 2 })
+      // .attr("transform", function(d) { return "translate(" + source.x0 + "," + source.y0 + ")"; })
+    .append("xhtml:body")
+    .append("xhtml:div")
+      .text(getNodeText)
+      .attr('class', 'caseText')
+
+  // Transition nodes to their new position.
+  var nodeUpdate = node.transition()
+      .duration(duration)
+      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+  nodeUpdate.select("rect")
+      .attr('y', 0)
+      .attr('x', function(d) { return -getNodeWidth(d) / 2 })
+      .attr('height', getNodeHeight)
+      .attr('width', getNodeWidth)
+      .style("fill", function(d) {
+        return d._children ? "lightsteelblue" : "#fff";
+      });
+
+  nodeUpdate.select("foreignObject")
+      .attr('x', function(d) { return -getNodeWidth(d) / 2 })
+      .text(getNodeText);
+
+  // Transition exiting nodes to the parent's new position.
+  var nodeExit = node.exit().transition()
+      .duration(duration)
+      .attr("transform", function(d) { return "translate(" + source.x + "," + source.y + ")"; })
+      .remove();
+
+  nodeExit.select("rect")
+      .attr('y', -nodeHeight / 2)
+      .attr('height', getNodeHeight)
+      .attr('width', getNodeWidth);
+
+  // nodeExit.select("text")
+  //     .style("fill-opacity", 1e-6);
+
+  // Update the links…
+  var link = svg.selectAll("path.link")
+      .data(links, function(d) { return d.target.id; });
+
+  // Enter any new links at the parent's previous position.
+  link.enter().insert("path", "g")
+      .attr("class", "link")
+      .attr("d", function(d) {
+        var o = {x: source.x0, y: source.y0 + getNodeHeight(d)};
+        return diagonal({source: o, target: o});
+      });
+
+  // Transition links to their new position.
+  link.transition()
+      .duration(duration)
+      .attr("d", diagonal);
+
+  // Transition exiting nodes to the parent's new position.
+  link.exit().transition()
+      .duration(duration)
+      .attr("d", function(d) {
+        var o = {x: source.x, y: source.y + getNodeHeight(d)};
+        return diagonal({source: o, target: o});
+      })
+      .remove();
+
+  // Stash the old positions for transition.
+  nodes.forEach(function(d) {
+    d.x0 = d.x;
+    d.y0 = d.y;
+  });
+
+  console.log('lalal');
+
+}
+
+// Toggle children on click.
+function click(d) {
+  if (d.children) {
+    d._children = d.children;
+    d.children = null;
+  } else {
+    d.children = d._children;
+    d._children = null;
+  }
+  update(d);
+}
