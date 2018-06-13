843cb4e09b807a2ab20327bd0e20dfd85b574d73
Milagro Teruel <milagro.teruel@gmail.com>
2018-06-13 09:24:00 -0300

    Adding TextChart

---
+++ attention/js/attentionViz.js
@@ -0,0 +1,10 @@
+var nouns = [
+  ['abcdefg', 2, 0], ['lolo', 3, 0], ['l', 4, 0],
+  ['abcdefg', 6, 0], ['lolo', 3, 0], ['l', 4, 0],
+  ['abcdefg', 2, 0], ['lolo', 3, 0], ['l', 4, 0],
+  ['abcdefg', 2, 0], ['lolo', 3, 0], ['laaaaaaaaaaaaaaaaaaaa', 4, 0],
+  ['abcdefg', 2, 0], ['lolo', 3, 0], ['laaaaaaaaaaaaaaaaaaaa', 10, 0],
+  ['abcdefg', 2, 0], ['lolo', 3, 1], ['leeeeeeeeee', 4, 1]];
+
+chart = new TextChart(nouns);
+chart.draw("text-container");
